2cd9f6c5d925754da4af39f007e293e67dee3128
EasonCSjourney <eason.c.hu@gmail.com>
2022-09-10 21:33:55 -0700

    added code

---
+++ 1.java
@@ -0,0 +1,18 @@
+public class DrawTriangle {
+    public static void main(String[] args) {
+        // Add code here!
+         int col = 0;
+         int row = 0;
+         int size = 5;
+         
+         while (row < size){
+             row = row + 1;
+             col = 0;
+             while(col < row){
+                 col = col + 1;
+                 System.out.print('*');       
+             }
+            System.out.println( );
+         }
+       }
+    }
